7f5ff4352b5d8286ab380ad7ae19c0e7bb055108
Brian J. Murrell <brian.murrell@intel.com>
2020-03-22 17:58:18 -0400

    is-avail, not avail
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/util/mpio_utils.py
+++ src/tests/ftest/util/mpio_utils.py
@@ -56,7 +56,7 @@ class MpioUtils():
                 '''set -x
                    export MODULEPATH=/usr/share/modules
                    for mod in mpi/mpich-x86_64 gnu-mpich; do
-                       if module avail $mod; then
+                       if module is-avail $mod; then
                            module load $mod
                            echo "Loaded $mod"
                            break
