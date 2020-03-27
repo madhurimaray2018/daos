8c71e1b2844572ce8e4a980818c386ec10002807
Kris Jacque <kristin.jacque@intel.com>
2020-03-27 17:20:31 -0600

    WIP - pool/cont svc
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

--- src/mgmt/srv_pool.c
+++ src/mgmt/srv_pool.c
@@ -1120,10 +1120,79 @@ out:
 	return rc;
 }
 
+static int
+cont_set_prop(uuid_t pool_uuid, uuid_t cont_uuid, daos_prop_t *prop)
+{
+	int		rc = 0;
+	d_rank_list_t	*ranks;
+	struct mgmt_svc	*svc;
+
+	rc = ds_mgmt_svc_lookup_leader(&svc, NULL /* hint */);
+	if (rc != 0)
+		goto out;
+
+	rc = pool_get_svc_ranks(svc, pool_uuid, &ranks);
+	if (rc != 0)
+		goto out_svc;
+
+	/* Container svc colocated with its pool svc */
+	rc = ds_pool_svc_cont_set_prop(pool_uuid, cont_uuid, ranks, prop);
+	if (rc != 0)
+		goto out_ranks;
+
+out_ranks:
+	d_rank_list_free(ranks);
+out_svc:
+	ds_mgmt_svc_put_leader(svc);
+out:
+	return rc;
+}
+
 int
 ds_mgmt_cont_set_owner(uuid_t pool_uuid, uuid_t cont_uuid, const char *user,
 		       const char *group)
 {
-	/* TODO - implement */
-	return 0;
+	int		rc = 0;
+	daos_prop_t	*prop;
+	uint32_t	prop_nr = 0;
+	uint32_t	i = 0;
+
+	D_DEBUG(DB_MGMT, "Setting property for container "DF_UUID" in pool "
+		DF_UUID"\n", DP_UUID(cont_uuid), DP_UUID(pool_uuid));
+
+	if (user != NULL)
+		prop_nr++;
+	if (group != NULL)
+		prop_nr++;
+	if (prop_nr == 0) {
+		D_ERROR("user and group both null\n");
+		return -DER_INVAL;
+	}
+
+	prop = daos_prop_alloc(prop_nr);
+	if (prop == NULL)
+		return -DER_NOMEM;
+
+	if (user != NULL) {
+		prop->dpp_entries[i].dpe_type = DAOS_PROP_CO_OWNER;
+		D_STRNDUP(prop->dpp_entries[i].dpe_str, user,
+			  DAOS_ACL_MAX_PRINCIPAL_LEN);
+		if (prop->dpp_entries[i].dpe_str == NULL)
+			D_GOTO(out_prop, rc = -DER_NOMEM);
+		i++;
+	}
+
+	if (group != NULL) {
+		prop->dpp_entries[i].dpe_type = DAOS_PROP_CO_OWNER_GROUP;
+		D_STRNDUP(prop->dpp_entries[i].dpe_str, group,
+			  DAOS_ACL_MAX_PRINCIPAL_LEN);
+		if (prop->dpp_entries[i].dpe_str == NULL)
+			D_GOTO(out_prop, rc = -DER_NOMEM);
+		i++;
+	}
+
+	rc = cont_set_prop(pool_uuid, cont_uuid, prop);
+out_prop:
+	daos_prop_free(prop);
+	return rc;
 }
